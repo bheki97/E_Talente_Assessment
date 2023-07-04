59a87460028ebf0548d8f89405325379796a4d01
bheki97 <bfmokoena7@gmail.com>
2023-07-04 15:24:09 +0200

    Initial commit

---
+++ src/main/java/com/eviro/assesment/grad001/bhekimautjana/ETalenteAssessmentApplication.java
@@ -0,0 +1,13 @@
+package com.eviro.assesment.grad001.bhekimautjana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ETalenteAssessmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ETalenteAssessmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eviro/assesment/grad001/bhekimautjana/ETalenteAssessmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eviro.assesment.grad001.bhekimautjana;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ETalenteAssessmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
